001b332d93a4732d7e735df96a12d2b3c583860a
yaminmhd <mdyamin.drzee@gmail.com>
2018-04-03 10:22:33 +0800

    Implemented palindrome to test cases

--- test/isPalindrome.test.js
+++ test/isPalindrome.test.js
@@ -6,4 +6,42 @@
 // isPalindrome('abba') --> true
 // isPalindrome('tacocat') --> true
 // isPalindrome('racecar') --> true
-// isPalindrome('umbrella') --> false
+// isPalindrome('umbrella') --> false isPalindrome from "../src/isPalindrome"
+
+
+const isPalindrome = require("../src/isPalindrome")
+it('empty string should return true', () => {
+  expect(isPalindrome('')).toEqual(true);
+});
+
+it('string with length of 1 should return true', () => {
+  expect(isPalindrome("a")).toEqual(true);
+});
+
+it('should return false when the length of the input is even', () => {
+  expect(isPalindrome('ab')).toEqual(false);
+});
+
+it('should return true when the length of the input is even but the have the same character', () => {
+  expect(isPalindrome('aa')).toEqual(true);
+});
+
+it('should return false when the length of the input is odd when the string is not palindrome', () => {
+  expect(isPalindrome("abc")).toEqual(false);
+});
+
+it('should return true when the length of the input is odd when the string is palindrome', () => {
+  expect(isPalindrome("aba")).toEqual(true);
+});
+
+it('should return true for tacocat', () => {
+  expect(isPalindrome('tacocat')).toEqual(true);
+});
+
+it('should return true for racecar', () => {
+  expect(isPalindrome('racecar')).toEqual(true);
+});
+
+it('should return true for umbrella', () => {
+  expect(isPalindrome('umbrella')).toEqual(false);
+});
